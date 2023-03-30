dc29bdf604fcc796df5b36366c7c8531ffa0861c
nalouddin <nalouddin@gmail.com>
2023-03-30 17:21:47 +0500

    Category nomli model yaratildi va madil admin qismi sozlandi

--- news/models.py
+++ news/models.py
@@ -8,7 +8,8 @@ class News(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Maqola joylashtrilgan vaqt')
     updated_at = models.DateTimeField(auto_now=True, verbose_name = 'Taxrirlangan vaqti')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name = 'Rasm')
-    is_published = models.BooleanField(default=True, verbose_name = 'Nash qilingan')
+    is_published = models.BooleanField(default=True, verbose_name = 'Nashr qilingan')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="bo'limlar")
 
     def __str__(self) -> str:
         return str(self.title)
@@ -16,3 +17,15 @@ class News(models.Model):
     class Meta:
         verbose_name = 'Yangliklar'
         verbose_name_plural = 'Yanglik'
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name="Bo'limlar nomi")
+
+    def __str__(self) -> str:
+        return str(self.title)
+    
+    class Meta:
+        verbose_name = "Yo'nalishlar"
+        verbose_name_plural = "Yo'nalishlar"
+        ordering = ['title']
+        
