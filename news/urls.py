6872f079011dfeb644520821567c5a5efd42361b
nalouddin <nalouddin@gmail.com>
2023-03-31 14:15:28 +0500

    Shablonni sozlandi

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomeView, get_category
 
 urlpatterns = [
+    path('category/<int:category_id>/', get_category, name = 'category'),
     path('', HomeView, name = 'home'),
-    path('cat/<int:category_id>/', get_category, name = 'category'),
 ]
