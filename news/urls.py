c4fda81e0436988c389886d8a48edb807cf1ff91
nalouddin <nalouddin@gmail.com>
2023-03-31 12:34:16 +0500

    categoriyaga bo'lib templates ga chiqarildi

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, get_category
 
 urlpatterns = [
-    path('', HomeView, name = 'home')
+    path('', HomeView, name = 'home'),
+    path('category/<int:category_id>/', get_category),
 ]
